12074c3a1dd4b993de5241ebb8fce2bee5d3a0de
Resaki1 <christian.reski@t-online.de>
2023-03-23 20:34:50 +0100

    WIP: added search input

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,7 +11,44 @@ export default component$(() => {
       <Link href="/">
         <Logo />
       </Link>
-      <Link href="/products">Suche</Link>
+      <div class="header_search">
+        <input type="text" placeholder="Suche Produkte" />
+        <div class="header_search-button">
+          <Link href="/products" class="header_search-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+          </Link>
+        </div>
+      </div>
+      <Link href="/products">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="8" cy="21" r="1"></circle>
+          <circle cx="19" cy="21" r="1"></circle>
+          <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"></path>
+        </svg>
+      </Link>
     </header>
   );
 });
